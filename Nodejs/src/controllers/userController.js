99f7c534462e1ebaa2c31da499d51d443d1c92d9
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-29 20:57:52 +0700

    rest api CRUD

--- Nodejs/src/controllers/userController.js
+++ Nodejs/src/controllers/userController.js
@@ -38,7 +38,31 @@ let handleGetAllUser = async (req, res) => {
   });
 };
 
+let handleCreateNewUser = async (req, res) => {
+  let message = await userServices.createNewUser(req.body);
+  return res.status(200).json({ message });
+};
+
+let handleDeleteUser = async (req, res) => {
+  if (!req.body.id) {
+    return res.status(100).json({
+      errCode: 1,
+      errMessage: "Missing required parameters!",
+    });
+  }
+  let message = await userServices.deleteUser(req.body.id);
+  return res.status(200).json({ message });
+};
+
+let handleEditUser = async (req, res) => {
+  let data = req.body;
+  let message = await userServices.updateUserData(data);
+  return res.status(200).json({ message });
+};
 module.exports = {
   handleLogin: handleLogin,
   handleGetAllUser: handleGetAllUser,
+  handleCreateNewUser: handleCreateNewUser,
+  handleEditUser: handleEditUser,
+  handleDeleteUser: handleDeleteUser,
 };
