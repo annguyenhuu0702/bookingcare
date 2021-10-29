adab5c881dee88b2f16c6b49ff1f42c4aa8c1669
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-29 19:13:06 +0700

    render users

--- Reactjs/src/containers/System/UserManage.js
+++ Reactjs/src/containers/System/UserManage.js
@@ -1,19 +1,124 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect, Fragment } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
+import "./userManage.scss";
+import { getAllUsers } from "./../../services/userService";
 
-function UserManage() {
-  return <div className="text-center">Manage users</div>;
-}
-// class UserManage extends Component {
-//   state = {};
+// function UserManage() {
+//   const [allUser, setAllUser] = useState([]);
 
-//   componentDidMount() {}
+//   useEffect(() => {
+//     async function fetchUsers() {
+//       let response = await getAllUsers("ALL");
+//       if (response && response.errCode === 0) {
+//         setAllUser(response.users);
+//       }
+//     }
+//     fetchUsers();
+//   }, []);
 
-//   render() {
-//     return <div className="text-center">Manage users</div>;
-//   }
+//   useEffect(() => {
+//     getAllUsers("ALL").then((response) => setAllUser(response.users));
+//   }, []);
+
+//   return (
+//     <div className="users-container">
+//       <div className="title mt-3 mx-1">Manage users</div>
+//       <div className="users-table">
+//         <table id="customers">
+//           <tr>
+//             <th>Email</th>
+//             <th>First name</th>
+//             <th>Last name</th>
+//             <th>Address</th>
+//             <th>Actions</th>
+//           </tr>
+//           <>
+//             {allUser &&
+//               allUser.map((item, index) => {
+//                 return (
+//                   <tr key={index}>
+//                     <td>{item.email}</td>
+//                     <td>{item.firstName}</td>
+//                     <td>{item.lastName}</td>
+//                     <td>{item.address}</td>
+//                     <td>
+//                       <button className="btn-edit" type="">
+//                         <i className="fas fa-pencil-alt"></i>
+//                       </button>
+//                       <button className="btn-delete" type="">
+//                         <i className="fas fa-trash"></i>
+//                       </button>
+//                     </td>
+//                   </tr>
+//                 );
+//               })}
+//           </>
+//         </table>
+//       </div>
+//     </div>
+//   );
 // }
+
+class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
+
+  async componentDidMount() {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
+    }
+  }
+
+  render() {
+    let arrUsers = this.state.arrUsers;
+
+    return (
+      <div className="users-container">
+        <div className="title mt-3 mx-1">Manage users</div>
+        <div className="users-table">
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Address</th>
+              <th>Actions</th>
+            </tr>
+            <>
+              {arrUsers &&
+                arrUsers.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item.email}</td>
+                      <td>{item.firstName}</td>
+                      <td>{item.lastName}</td>
+                      <td>{item.address}</td>
+                      <td>
+                        <button className="btn-edit" type="">
+                          <i className="fas fa-pencil-alt"></i>
+                        </button>
+                        <button className="btn-delete" type="">
+                          <i className="fas fa-trash"></i>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {};
